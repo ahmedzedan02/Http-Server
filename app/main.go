4766cfa3abaff7be7fa2081517b4e578cabf6ee0
ahmedzedan02 <wapanda90@gmail.com>
2025-10-28 15:47:24 +0300

    [Return a file ]

--- app/main.go
+++ app/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
+
+
 	//"path"
 	"strings"
 )
@@ -14,6 +17,14 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
+	var directory  string
+	args:=os.Args
+	for i,arg :=range args {
+		if arg =="--directory" && i+1<len(args){
+			directory=args[i+1]
+		}
+
+	}
 	fmt.Println("Server is running on port 4221...")
 	listener,err :=net.Listen("tcp","0.0.0.0:4221")
 	if err !=nil{
@@ -28,12 +39,13 @@ func main() {
 			fmt.Println("Error accepting connection:", err)
 			continue
 		}
-		go handleConnection(conn)
+		//Concurrent connections
+		go handleConnection(conn,directory)
 	}
 }
 
 	
-	func handleConnection(conn net.Conn){
+	func handleConnection(conn net.Conn,directory string){
 		defer conn.Close()
 
 	
@@ -50,6 +62,7 @@ func main() {
 	}
 	path :=parts[1]
 	var response string 
+	// Handle endpoints
 	if path == "/" {
 	response = "HTTP/1.1 200 OK\r\n\r\n"
 }else if strings.HasPrefix(path,"/echo/") {
@@ -59,13 +72,26 @@ response = fmt.Sprintf(
 				"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
 				contentlen, message)
 
-} else {
+}else if strings.HasPrefix(path,"/files/"){
+	filename:=strings.TrimPrefix(path,"/files/")
+	filepath:=filepath.Join(directory ,filename) 
+
+	// Check if file exists
+	data,err :=os.ReadFile(filepath)
+	if err !=nil {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}else{
+		response = fmt.Sprintf(
+				"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s",
+				len(data), string(data))
+	}	
+
+}else {
 	response = "HTTP/1.1 404 Not Found\r\n\r\n"
 }
 
 
 conn.Write([]byte(response))
-conn.Close()
 	
 
 	}
